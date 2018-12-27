661b931a375a148197673c716a73530310bf49db
Eugene Yokota <eed3si9n@gmail.com>
2018-12-26 20:54:49 -0500

    sync

---
+++ script/rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p www
+pf recipe/ www/
+cd www
+tar zcf ../recipes.tar.gz ./
+cd ..
+scp -i ~/.ssh/portal.pem recipes.tar.gz bitnami@eed3si9n.com:/home/bitnami/work/recipes.tar.gz
+ssh bitnami@www.eed3si9n.com -i ~/.ssh/portal.pem 'tar zxvf /home/bitnami/work/recipes.tar.gz -C /home/bitnami/apps/portal/htdocs/recipes/'
